4d0b047fda1cc32b323991b22e8122224652ff67
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-02-13 15:09:49 -0500

    updated classes

--- index.js
+++ index.js
@@ -1,57 +1,55 @@
-onst inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
-const fs = require('fs');
+const Employee = require("./lib/Employee")
 
+//Initialize new Employee object
+const employee = new Employee(); 
 
-
+const roles = ['Manager','Engineer', 'Intern']
 
 // TODO: Create an array of questions for user input
-const questions = [
+const employeeQuestions = [
       {
         type: 'input',
-        message: 'What is the name of project',
-        name: 'projectname',
+        message: 'What is the team member name?',
+        name: 'name',
       },
       {
         type: 'input',
-        message: 'What is a description of the project?',
-        name: 'projectdescription',
+        message: 'What is their employee ID?',
+        name: 'id',
       },
       {
         type: 'input',
-        message: 'What are the insallation instructions?',
-        name: 'installinstructions',
-      },
-      {
-        type: 'input',
-        message: 'What is your projects inteded use?',
-        name: 'intendeduse',
-      },
-      {
-        type: 'input',
-        message: 'What are your projects collaborators or 3rd party assets?',
-        name: 'contributing',
+        message: 'What is their email?',
+        name: 'email',
       },
       {
         type: 'list',
-        message: 'What is your project license?',
-        choices: licensesObj,
-        name: 'license'
+        message: 'What is their role?',
+        choices: roles,
+        name: 'role',
       },
       {
         type: 'input',
-        message: 'What is your github username?',
-        name: 'gitusername',
+        message: 'What is their office number?',
+        name: 'officenumber',
+        when: employee => employee.role === "Manager",
       },
       {
         type: 'input',
-        message: 'What is your contact email?',
-        name: 'emailcontact',
-      },    
+        message: 'What is their GitHub Username?',
+        name: 'gitname',
+        when: employee => employee.role === "Engineer"
+      },
+      {
+        type: 'input',
+        message: 'What is their school?',
+        name: 'school',
+        when: employee => employee.role === "Student"
+      },
     ];
 
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write to HTML
 function writeToFile(data) {
   const fileName = "./readmefolder/README.md";
 
@@ -66,7 +64,7 @@ function writeToFile(data) {
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions)
+  inquirer.prompt(emplpyeeQuestions)
   .then (answers => writeToFile(generateMarkdown(answers)))
 }
 
